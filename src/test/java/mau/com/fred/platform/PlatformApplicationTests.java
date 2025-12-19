997ebb42ed8fd1cd62d9f9fe2f22a6e42361b57f
binyamasrat <binyam.asrat_contr@fbinv.com>
2025-12-19 12:42:53 +0300

    -bootstrap project

---
+++ src/test/java/mau/com/fred/platform/PlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package mau.com.fred.platform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlatformApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
